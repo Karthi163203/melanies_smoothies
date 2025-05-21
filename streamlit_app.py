e191155a27a57d19bd5bdc3c8281879b77dadad5
Karthi163203 <karthi163203@mastek.com>
2025-05-21 10:22:37 +0530

    Update streamlit_app.py
    Added search_on column

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col, when_matched
 import requests
 
-
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw: ")
 st.write(
